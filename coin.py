b8f568bcf41f5a604396375a024fd494968ea6e6
u3814520 <u3814520@gmail.com>
2021-10-03 13:11:44 +0800

    coin_calculate

---
+++ coin.py
@@ -0,0 +1,14 @@
+one=int(input("請輸入1元有幾個?"))
+f=int(input("請輸入5元有幾個?"))
+t=int(input("請輸入10有幾個?"))
+ft=int(input("請輸入50元有幾個?"))
+
+ten=t*10
+five=f*5
+fifty=ft*50
+total=ten+five+fifty+one
+
+print("總共是{}元".format(total))
+
+input()
+
